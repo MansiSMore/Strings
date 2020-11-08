1bf2f59a6f8be31cc08340f968344b45ef70c412
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2020-11-08 16:42:31 +0530

    Create print_first_letter_of_every_word.cpp

---
+++ print_first_letter_of_every_word.cpp
@@ -0,0 +1,41 @@
+/*
+Given a string S, the task is to create a string with the first letter of every word in the string.
+
+Example 1:
+
+Input: 
+S = "geeks for geeks"
+Output: gfg
+
+Example 2:
+
+Input: 
+S = "bad is good"
+Output: big
+
+Your Task:
+You don't need to read input or print anything. 
+Your task is to complete the function firstAlphabet() which takes string S as input parameter and returns a string that contains the first letter of every word in S.
+
+Expected Time Complexity: O(|S|)
+Expected Auxiliary Space: O(|S|)
+
+Constraints:
+1 <= |S| <= 105
+S contains lower case English alphabets
+Words have a single space between them.
+*/
+
+string firstAlphabet(string S)
+	{
+	    string res;
+	    res += S[0];
+	    for(int i = 1 ;i < S.length() - 1; i++)
+	    {
+	        if(S[i] == ' ')
+	        {
+	            res += S[i + 1];
+	        }
+	    }
+	    return res;
+	}
